d3c38d3dcbb2de2e6dc5907030e48b25ccd8a142
Alejandro Gómez Huertas <83892847+alejo14171@users.noreply.github.com>
2024-01-15 01:48:46 +0000

    Just trying to commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -10,22 +11,55 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
 
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear proyecto',
-    'Crear componentes',
-    'Crear servicios',
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear servicios',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Hacer Deploy',
+      completed: false
+    }
   ]);
 
   chengeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.tasks.update((tasks) => [...tasks, newTask]); //crea un nuevo estado
+    this.addTask(newTask);
     input.value = "";
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]); //crea un nuevo estado
   }
 
   deleteTask(index: number) {
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
+  updateTask(index: number) {
+    // this.tasks.update(
+    //   (tasks) => {
+        
+    //   }
+    // )
   }
 
 }
